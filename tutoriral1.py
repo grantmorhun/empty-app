11c2ef9572d33cfb4492cac4dae873a4afd3f8d9
grantmorhun <grantmorhun@hanovernorwichschools.org>
2015-09-15 14:46:19 -0400

    Updated from Brython Server: 9/15/2015 2:46:19 PM

--- tutoriral1.py
+++ tutoriral1.py
@@ -13,7 +13,7 @@ black = Color(0x000000, 1.0)
 # Define a line style that is a thin (1 pixel) wide black line
 thinline = LineStyle(1, black)
 # A graphics asset that represents a rectangle
-polygon = PolygonAsset(100,150,100,150)
+polygon = PolygonAsset{[(100,150), (300,20), (1,2)]}
 
 # Now display a rectangle
 Sprite(polygon)
