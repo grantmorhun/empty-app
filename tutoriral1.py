833bbbf5ff3262a6d4d5618a3f80577ffe884aa6
grantmorhun <grantmorhun@hanovernorwichschools.org>
2015-09-15 14:19:07 -0400

    Updated from Brython Server: 9/15/2015 2:19:07 PM

--- tutoriral1.py
+++ tutoriral1.py
@@ -13,7 +13,7 @@ black = Color(0x000000, 1.0)
 
 thinline = LineStyle(1,black)
 
-Ellipse = EllipseAsset(50, 20, thinline, blue)
+Ellipse = EllipseAsset(1000, 20, thinline, blue)
 Sprite(ellipse)
 Ellipse = EllipseAsset(40, 10, thinline, blue)
 
