0384add2c09dc73b50745203d3489202cf10fa9a
grantmorhun <grantmorhun@hanovernorwichschools.org>
2015-09-15 14:31:14 -0400

    Updated from Brython Server: 9/15/2015 2:31:13 PM

--- tutoriral1.py
+++ tutoriral1.py
@@ -16,6 +16,8 @@ thinline = LineStyle(1,black)
 Ellipse = EllipseAsset(100, 20, thinline, blue)
 Sprite(Ellipse,(100,100))
 
+Polygon=PolygonAsset(100, 20, thinline, red)
+Sprite(Polygon, (100,100))
 
 myapp = App()
 myapp.run()
