6760dd85c0b6a34030daf066478d72a607266ab9
grantmorhun <grantmorhun@hanovernorwichschools.org>
2015-09-15 14:17:09 -0400

    Updated from Brython Server: 9/15/2015 2:17:09 PM

--- tutoriral1.py
+++ tutoriral1.py
@@ -13,9 +13,9 @@ black = Color(0x000000, 1.0)
 
 thinline = LineStyle(1,black)
 
-rectangle = RectangleAsset(50, 20, thinline, blue)
+elipse = ElipseAsset(50, 20, thinline, blue)
 Sprite(rectangle)
-rectangle = RectangleAsset(40, 10, thinline, blue)
+elipse = ElipseAsset(40, 10, thinline, blue)
 
 Sprite(rectangle)
 
