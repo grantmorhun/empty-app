fc16c3baef56238c10a2552eb58d010b6ca7ba33
grantmorhun <grantmorhun@hanovernorwichschools.org>
2015-09-15 14:31:52 -0400

    Updated from Brython Server: 9/15/2015 2:31:52 PM

--- tutoriral1.py
+++ tutoriral1.py
@@ -15,7 +15,6 @@ thinline = LineStyle(1,black)
 
 Ellipse = EllipseAsset(100, 20, thinline, blue)
 Sprite(Ellipse,(100,100))
-
 Polygon=PolygonAsset(100, 20, thinline, red)
 Sprite(Polygon, (100,100))
 
